2a486480ea84aab7bae1fa80f05ff86e2b96522b
blackbrokkoli <kastomat@gmail.com>
2023-07-20 19:38:57 +0200

    implement basic frontmatter readout with syntax i got from discord help

--- main.ts
+++ main.ts
@@ -30,40 +30,27 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'manipulate-front-matter',
+			name: 'Manipulate Front Matter',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// get the current active file
+				const activeFile = this.app.workspace.getActiveFile();
+				if (!activeFile) {
+					new Notice('No active file found...');
+					return;
 				}
+				// read out the frontmatter
+				console.log('FILE CACHE', app.metadataCache.getFileCache(activeFile))
+				const frontmatter = app.metadataCache.getFileCache(activeFile)?.frontmatter
+
+				if (!frontmatter) {
+					new Notice('No frontmatter found...');
+					return;
+				}
+				new Notice(`Frontmatter: ${JSON.stringify(frontmatter)}`);
 			}
 		});
+	
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
